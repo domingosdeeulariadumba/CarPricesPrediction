cde980a52514292d59f21e1a50f5c57036b3ace6
Domingos de Eulária Dumba <110714056+domingosdeeulariadumba@users.noreply.github.com>
2024-10-15 20:48:14 +0100

    Update eval_models.py

--- eval_models.py
+++ eval_models.py
@@ -1,5 +1,3 @@
-
-
 '''
         IMPORTING MODULES
         ------------
@@ -8,119 +6,253 @@
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import RobustScaler
 from sklearn.feature_selection import RFE
-from sklearn.model_selection import train_test_split as tts, cross_val_score as cv, GridSearchCV
-from sklearn.linear_model import LinearRegression as lreg, Lasso, Ridge, ElasticNet
-from sklearn import tree
-from sklearn.ensemble import RandomForestRegressor as rfr
-import xgboost as xgb
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as imb_pipeline
+from sklearn.metrics import (accuracy_score, precision_score,
+                             recall_score, f1_score, roc_auc_score)
 
 
 
-'''
-        Implementation
-        --------------
-'''
-
-
+    # Regression Problems
+    
+# Function to return scores of candidate regressors and the best pipeline
 def eval_reg(candidate_models, X_train, y_train):
     
-    """
-    Evaluate candidate regression models using cross-validation and return sorted scores.
-
-    This function loops through a set of candidate models, applies scaling and feature selection,
-    performs cross-validation, and computes the mean cross-validated score for each model. For specific
-    models like ElasticNet and XGBoost, it also performs hyperparameter tuning using GridSearchCV.
-
-    Parameters
-    ----------
-    candidate_models : dict
-        A dictionary of model names (as keys) and corresponding instantiated model objects (as values).
-        Example: {'OLS': LinearRegression(), 'Decision Tree': DecisionTreeRegressor(), 'XGB': XGBRegressor(), etc.}
+    '''
+    Evaluate candidate regression models and return the best model pipeline.
     
-    X_train : array-like
-        Training data features. This is the matrix of independent variables (predictors).
+    Parameters:
+    candidate_models (dict): A dictionary of candidate regression models.
+    X_train (DataFrame): Training features.
+    y_train (Series): Training target.
+
+    Returns:
+    tuple: DataFrame of model scores and the best pipeline.
+    '''
     
-    y_train : array-like
-        Target variable corresponding to X_train. This is the dependent variable (response) that the models
-        aim to predict.
-
-    Returns
-    -------
-    pd.DataFrame
-        A pandas DataFrame with two columns:
-            - 'Model': The name of each model.
-            - 'Mean Score': The mean cross-validated score for each model (negative mean squared error).
-        The DataFrame is sorted by the 'Mean Score' in descending order.
-
-    Notes
-    -----
-    - RobustScaler is used to scale the features.
-    - RFE (Recursive Feature Elimination) is applied for feature selection.
-    - Models like ElasticNet and XGBoost undergo hyperparameter tuning using GridSearchCV.
-    - For other models, only the alpha parameter is tuned via GridSearchCV.
-    - Cross-validation is performed using the 'neg_mean_squared_error' scoring metric.
-
-    Example
-    -------
-    >>> from sklearn.linear_model import ElasticNet
-    >>> from sklearn.tree import DecisionTreeRegressor
-    >>> from xgboost import XGBRegressor
-    >>> models = {
-    >>>     'OLS': LinearRegression(),
-    >>>     'Decision Tree': DecisionTreeRegressor(),
-    >>>     'XGB': XGBRegressor(),
-    >>>     'ElasticNet': ElasticNet()
-    >>> }
-    >>> df_results = eval_reg(models, X_train, y_train)
-    >>> print(df_results)
+    # Empty dictionary to store candidate models scores
+    dict_scores = {} 
     
-    """
-    
-    dict_scores = {}
+    # Function to retrieve the appropriate feature reduction
+    def feat_red_strategy(X_train):
+        
+        if X_train.shape[1] > 99:
+            pca = PCA().fit(X_train)   
+            n_comp = np.argmax(pca.explained_variance_ratio_.cumsum() >= 0.95) + 1
+            return 'pca', PCA(n_components = n_comp)
+        else:
+            return 'feature selection', ''
 
-    param_grid = {'regressor__alpha': [0.1, 1, 10],
-                  'regressor__l1_ratio': [0.1, 0.5, 0.9]}
-
-    param_grid_xgb = {'regressor__lambda': [0.1, 1, 10],
-                      'regressor__alpha': [0.1, 1, 10]}
-
-    for model_name, model_instance in candidate_models.items():
-        model_pipeline = Pipeline(steps=[
-            ('scaler', RobustScaler()),
-            ('feature_selection', RFE(model_instance)),
+    # Looping through different candidate models for evaluation
+    for model_name, model_instance in candidate_models.items(): 
+        feat_red_strategy_name, feat_red_strategy_model = feat_red_strategy(X_train)        
+        if feat_red_strategy_name == 'pca':
+            feat_red_strategy_step = ('pca', feat_red_strategy_model)
+        else:            
+            feat_red_strategy_step = ('feature selection', RFE(model_instance))
+            
+        # Building the pipeline for each model    
+        model_pipeline = Pipeline(steps = [
+            feat_red_strategy_step,
             ('regressor', model_instance)
-        ])
-
-        if model_name in ['OLS', 'Decision Tree', 'Random Forest']:
-            scores = cv(model_pipeline, X_train, y_train, scoring='neg_mean_squared_error')
-            dict_scores[model_name] = scores.mean()
-
-        elif model_name == 'XGB':
-            gridsearch_cv = GridSearchCV(model_pipeline, param_grid=param_grid_xgb,
-                                         scoring='neg_mean_squared_error')
-            gridsearch_cv.fit(X_train, y_train)
-            best_model = gridsearch_cv.best_estimator_
-            scores = cv(best_model, X_train, y_train, scoring='neg_mean_squared_error')
-            dict_scores[model_name] = scores.mean()
-
-        elif model_name == 'ElasticNet':
-            gridsearch_cv = GridSearchCV(model_pipeline, param_grid=param_grid, scoring='neg_mean_squared_error')
-            gridsearch_cv.fit(X_train, y_train)
-            best_model = gridsearch_cv.best_estimator_
-            scores = cv(best_model, X_train, y_train, scoring='neg_mean_squared_error')
+        ])        
+        
+        # Conditions for each candidate model
+        if model_name == 'OLS':
+            scores = cross_val_score(model_pipeline, 
+                                     X_train, y_train, 
+                                     scoring ='neg_mean_squared_error')
             dict_scores[model_name] = scores.mean()
 
         else:
-            gridsearch_cv = GridSearchCV(model_pipeline, param_grid={'regressor__alpha': param_grid['regressor__alpha']},
-                                         scoring='neg_mean_squared_error')
+            # Parameters setup
+            param_reg = {'regressor__alpha': [0.1, 1, 10],
+                          'regressor__l1_ratio': [0.1, 0.5, 0.9]}    
+            param_xgb = {'regressor__lambda': [0.1, 1, 10],
+                              'regressor__alpha': [0.1, 1, 10],
+                              'regressor__learning_rate': [0.1, 0.2, 0.3]}
+            tree_depths, min_imp_decs = [3, 5, 7], [0.001, 0.01, 0.1]     
+            param_tree = {'regressor__max_depth': tree_depths,
+                         'regressor__min_impurity_decrease': min_imp_decs}    
+            param_forest = {'regressor__max_depth': tree_depths}   
+            
+            if model_name in ['Lasso', 'Ridge']:
+                param_grid_ = {'regressor__alpha': param_reg['regressor__alpha']}
+            elif model_name == 'Elastic Net':
+                param_grid_ = param_reg
+            elif model_name == 'Decision Tree':
+                param_grid_ = param_tree
+            elif model_name == 'Random Forest':
+                    param_grid_ = param_forest
+            else:
+                param_grid_ = param_xgb       
+                
+            gridsearch_cv = GridSearchCV(model_pipeline, 
+                                         param_grid = param_grid_,
+                                         scoring = 'neg_mean_squared_error')
             gridsearch_cv.fit(X_train, y_train)
             best_model = gridsearch_cv.best_estimator_
-            scores = cv(best_model, X_train, y_train, scoring='neg_mean_squared_error')
+            scores = cross_val_score(best_model, X_train, y_train, 
+                        scoring = 'neg_mean_squared_error')
             dict_scores[model_name] = scores.mean()
 
-    df_scores = pd.DataFrame(list(dict_scores.items()), columns=['Model', 'Mean Score'])
-    df_scores.sort_values(by = 'Mean Score', ascending = False, inplace = True)
+    # Dataframe to store the candidate models results in descending order
+    df_scores = pd.DataFrame(list(dict_scores.items()), 
+                             columns = ['Model', 'Mean Score (MSE)'])
+    df_scores.sort_values(by = 'Mean Score (MSE)',
+                          ascending = False, inplace = True)
 
-    return df_scores
+    # Extracting the best model and its instance from sorted scores
+    best_model_name = df_scores.iat[0, 0]
+    best_model_instance = candidate_models[best_model_name]
+    
+    # Selecting the reduction strategy given the function above   
+    if feat_red_strategy_name == 'pca':
+        best_feat_red_strategy_step = feat_red_strategy_step
+    else:
+        best_feat_red_strategy_step = ('feature selection', 
+                                       RFE(best_model_instance))
+
+    # Building the best model pipeline with a valid feature reduction strategy
+    best_model_pipeline = Pipeline(steps = [
+        best_feat_red_strategy_step,
+        ('regressor', best_model_instance)
+    ])
+
+    # Tuple containing the candidate models result and the best pipeline
+    eval_data = (df_scores, best_model_pipeline)
+
+    return eval_data
+
+
+
+
+    # Classification Problems
+    
+# Function to return scores of candidate classifiers and the best pipeline
+def eval_class(candidate_models, X_train, y_train):
+    
+    '''
+    Evaluate candidate classification models and return the best model pipeline.
+    
+    Parameters:
+    candidate_models (dict): A dictionary of candidate regression models.
+    X_train (DataFrame): Training features.
+    y_train (Series): Training target.
+
+    Returns:
+    tuple: DataFrame of model scores and the best pipeline.
+    '''
+        
+    # Empty dictionary to store candidate models scores    
+    dict_scores = {}    
+    
+    # Function to retrieve the appropriate feature reduction
+    def feat_red_strategy(X_train):
+        
+        if X_train.shape[1] > 99:
+            pca = PCA().fit(X_train)   
+            n_comp = np.argmax(pca.explained_variance_ratio_.cumsum() >= 0.95) + 1
+            return 'pca', PCA(n_components = n_comp)
+        else:
+            return 'feature selection', ''
+
+    # Looping through different candidate models for evaluation
+    for model_name, model_instance in candidate_models.items(): 
+        feat_red_strategy_name, feat_red_strategy_model = feat_red_strategy(X_train)        
+        if feat_red_strategy_name == 'pca':
+            feat_red_strategy_step = (feat_red_strategy_name, feat_red_strategy_model)
+        else:            
+            feat_red_strategy_step = (feat_red_strategy_name, RFE(model_instance))
+            
+        # Building the pipeline for each model    
+        model_pipeline = imb_pipeline(steps = [
+            feat_red_strategy_step,
+            ('oversampling', SMOTE(random_state = 7)),
+            ('classifier', model_instance)
+            ])
+       
+        # Conditions for each candidate model
+        if model_name == 'Logistic Regression':
+            scores = cross_val_score(model_pipeline, X_train, y_train, scoring = 'f1')
+            dict_scores[model_name] = scores.mean()
+        else:
+            # Parameters setup
+            tree_depths, min_imp_decs = [3, 5, 7], [0.001, 0.01, 0.1]     
+            param_tree = {'classifier__max_depth': tree_depths,
+                         'classifier__min_impurity_decrease': min_imp_decs}    
+            param_forest = {'classifier__max_depth': tree_depths}   
+            param_ridge = {
+            'classifier__C': [0.1, 1, 10]}
+            
+            if model_name == 'Ridge':
+                param_grid_ = param_ridge
+            elif model_name == 'Decision Tree':
+                param_grid_ = param_tree
+            else:
+                param_grid_ = param_forest 
+                
+            # Grid Search Cross Validation
+            gridsearch_cv = GridSearchCV(model_pipeline, 
+                param_grid = param_grid_, scoring = 'f1')
+            gridsearch_cv.fit(X_train, y_train)
+            best_model = gridsearch_cv.best_estimator_
+            scores = cross_val_score(best_model, X_train, y_train,
+                        scoring = 'f1')
+            dict_scores[model_name] = scores.mean()
+            
+    # Dataframe to store the candidate models results in descending order
+    df_scores = pd.DataFrame(list(dict_scores.items()), 
+                             columns = ['Model', 'Mean Score (F1)'])
+    df_scores.sort_values(by = 'Mean Score (F1)',
+                          ascending = False, inplace = True)
+
+    # Extracting the best model and its instance from sorted scores
+    best_model_name = df_scores.iat[0, 0]
+    best_model_instance = candidate_models[best_model_name]
+    
+    # Selecting the reduction strategy given the function above   
+    if feat_red_strategy_name == 'pca':
+        best_feat_red_strategy_step = feat_red_strategy_step
+    else:
+        best_feat_red_strategy_step = ('feature selection', 
+                                       RFE(best_model_instance))
+
+    # Building the best model pipeline with a valid feature reduction strategy
+    best_model_pipeline = imb_pipeline(steps = [
+            best_feat_red_strategy_step,
+            ('oversampling', SMOTE(random_state = 7)),
+            ('classifier', best_model_instance)
+            ])
+
+    # Tuple containing the candidate models result and the best pipeline
+    eval_data = (df_scores, best_model_pipeline)
+
+    return eval_data
+
+
+# Function for retrieving confusion matrix and AUC-ROC
+def show_full_report(X_test, y_test, y_pred):
+
+    '''
+    Show the full report of classification metrics.
+    Parameters:
+        X_test (DataFrame): Testing features.
+        y_test (Series): True labels for the test set.
+        y_pred (array): Predicted labels.
+
+    Returns:
+        DataFrame: Scores of classification metrics.
+    '''
+    
+    precision, recall = precision_score(y_test, y_pred), recall_score(y_test, y_pred)
+    accuracy, f1 = accuracy_score(y_test, y_pred), f1_score(y_test, y_pred)
+    auc_roc = roc_auc_score(y_test, y_pred)
+
+    df_score = pd.DataFrame({'Score': [precision, recall, accuracy, f1, auc_roc]},
+             index = ['Precision', 'Recall', 'Accuracy', 'F1 Score', 'AUC ROC']).T
+    
+    return df_score
