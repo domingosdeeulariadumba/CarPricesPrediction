e7fec6f68e9d1ee10aab4413c32f4809d2dfabd0
Domingos de Eulária Dumba <110714056+domingosdeeulariadumba@users.noreply.github.com>
2024-10-19 09:14:51 +0100

    Update eval_models.py

--- eval_models.py
+++ eval_models.py
@@ -9,10 +9,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import RFE
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score, GridSearchCV
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline as imb_pipeline
-from sklearn.metrics import (accuracy_score, precision_score,
-                             recall_score, f1_score, roc_auc_score)
 
 
 
@@ -71,14 +67,15 @@ def eval_reg(candidate_models, X_train, y_train):
             # Parameters setup
             param_reg = {'regressor__alpha': [0.1, 1, 10],
                           'regressor__l1_ratio': [0.1, 0.5, 0.9]}    
-            param_xgb = {'regressor__lambda': [0.1, 1, 10],
-                              'regressor__alpha': [0.1, 1, 10],
-                              'regressor__learning_rate': [0.1, 0.2, 0.3]}
             tree_depths, min_imp_decs = [3, 5, 7], [0.001, 0.01, 0.1]     
             param_tree = {'regressor__max_depth': tree_depths,
                          'regressor__min_impurity_decrease': min_imp_decs}    
             param_forest = {'regressor__max_depth': tree_depths}   
-            
+            param_xgb = {'regressor__lambda': [0.1, 1, 10],
+                              'regressor__alpha': param_reg['regressor__alpha'],
+                              'regressor__learning_rate': [0.1, 0.2, 0.3],
+                              'regressor__max_depth': tree_depths,
+                              'regressor__gamma': [0, 0.1, 0.2, 0.5, 1]}            
             if model_name in ['Lasso', 'Ridge']:
                 param_grid_ = {'regressor__alpha': param_reg['regressor__alpha']}
             elif model_name == 'Elastic Net':
@@ -102,7 +99,7 @@ def eval_reg(candidate_models, X_train, y_train):
     # Dataframe to store the candidate models results in descending order
     df_scores = pd.DataFrame(list(dict_scores.items()), 
                              columns = ['Model', 'Mean Score (MSE)'])
-    df_scores.sort_values(by = 'Mean Score (MSE)',
+    df_scores.sort_values(by = df_scores.columns[-1],
                           ascending = False, inplace = True)
 
     # Extracting the best model and its instance from sorted scores
@@ -169,15 +166,21 @@ def eval_class(candidate_models, X_train, y_train):
             feat_red_strategy_step = (feat_red_strategy_name, RFE(model_instance))
             
         # Building the pipeline for each model    
-        model_pipeline = imb_pipeline(steps = [
+        model_pipeline = Pipeline(steps = [
             feat_red_strategy_step,
-            ('oversampling', SMOTE(random_state = 7)),
             ('classifier', model_instance)
             ])
        
+        # Scoring condition
+        if len(set(y_train)) > 2:
+            scoring_ = 'f1_micro'
+        else:
+            scoring_ = 'f1'       
+        
         # Conditions for each candidate model
         if model_name == 'Logistic Regression':
-            scores = cross_val_score(model_pipeline, X_train, y_train, scoring = 'f1')
+            scores = cross_val_score(model_pipeline, X_train,
+                                     y_train, scoring = scoring_)
             dict_scores[model_name] = scores.mean()
         else:
             # Parameters setup
@@ -185,29 +188,39 @@ def eval_class(candidate_models, X_train, y_train):
             param_tree = {'classifier__max_depth': tree_depths,
                          'classifier__min_impurity_decrease': min_imp_decs}    
             param_forest = {'classifier__max_depth': tree_depths}   
-            param_ridge = {
-            'classifier__C': [0.1, 1, 10]}
-            
-            if model_name == 'Ridge':
-                param_grid_ = param_ridge
-            elif model_name == 'Decision Tree':
-                param_grid_ = param_tree
+            svm_pipeline = Pipeline(steps = [step for i, step
+                                                 in enumerate(model_pipeline.steps) if i != 0])
+            param_svm = {'classifier__kernel': ['linear', 'poly', 'rbf',
+                                                'sigmoid'],
+                'classifier__C': [0.1, 1, 10, 100]}
+            param_xgb = {'classifier__lambda': [0.1, 1, 10],
+                              'classifier__alpha': [0.1, 1, 10],
+                              'classifier__learning_rate': [0.1, 0.2, 0.3],
+                              'classifier__max_depth': tree_depths,
+                              'classifier__gamma': [0, 0.1, 0.2, 0.5, 1]}            
+            if model_name == 'Decision Tree':
+                param_grid_, model_pipeline = param_tree, model_pipeline
+            elif model_name == 'SVM':
+                param_grid_, model_pipeline = param_svm, svm_pipeline
+            elif model_name == 'XGB':
+                param_grid_, model_pipeline = param_xgb, model_pipeline                       
             else:
-                param_grid_ = param_forest 
+                param_grid_, model_pipeline = param_forest, model_pipeline 
                 
             # Grid Search Cross Validation
             gridsearch_cv = GridSearchCV(model_pipeline, 
-                param_grid = param_grid_, scoring = 'f1')
+                param_grid = param_grid_, scoring = scoring_)
             gridsearch_cv.fit(X_train, y_train)
             best_model = gridsearch_cv.best_estimator_
             scores = cross_val_score(best_model, X_train, y_train,
-                        scoring = 'f1')
+                        scoring = scoring_)
             dict_scores[model_name] = scores.mean()
             
     # Dataframe to store the candidate models results in descending order
     df_scores = pd.DataFrame(list(dict_scores.items()), 
-                             columns = ['Model', 'Mean Score (F1)'])
-    df_scores.sort_values(by = 'Mean Score (F1)',
+                             columns = ['Model',
+                                        f'Mean Score ({scoring_.replace("_", " ").title()})'])
+    df_scores.sort_values(by = df_scores.columns[-1],
                           ascending = False, inplace = True)
 
     # Extracting the best model and its instance from sorted scores
@@ -222,9 +235,11 @@ def eval_class(candidate_models, X_train, y_train):
                                        RFE(best_model_instance))
 
     # Building the best model pipeline with a valid feature reduction strategy
-    best_model_pipeline = imb_pipeline(steps = [
+    if best_model_name == 'SVM':
+        best_model_pipeline = svm_pipeline         
+    else:     
+        best_model_pipeline = Pipeline(steps = [
             best_feat_red_strategy_step,
-            ('oversampling', SMOTE(random_state = 7)),
             ('classifier', best_model_instance)
             ])
 
